66d17589dd5e2f8fa3622cee74692099da2aa0b5
simba <simbamuzika@gmail.com>
2024-10-25 10:37:28 +0200

    refactor department-service

--- department-service/src/main/java/com/muzikacode/departmentservice/controller/DepartmentController.java
+++ department-service/src/main/java/com/muzikacode/departmentservice/controller/DepartmentController.java
@@ -1,9 +1,33 @@
 package com.muzikacode.departmentservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.muzikacode.departmentservice.model.Department;
+import com.muzikacode.departmentservice.repository.DepartmentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/department")
 public class DepartmentController {
+    private static final Logger LOGGER= LoggerFactory.getLogger(DepartmentController.class);
+    @Autowired
+    private DepartmentRepository repository;
+    @PostMapping
+    public Department add(@RequestBody Department department){
+        LOGGER.info("Department add: {}",department);
+        return repository.addDepartment(department);
+    }
+    @GetMapping
+    public List<Department> findAll(){
+        LOGGER.info("Department findAll");
+        return repository.findAll();
+    }
+    @GetMapping("/{id}")
+    public Department findById(@PathVariable Long id ){
+        LOGGER.info("Department find: id={}",id);
+     return repository.findById(id);
+    }
 }
